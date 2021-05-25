ae9f6ede07276199c6f5ad3576debdbf6154f1e4
kj <kristoferjoseph@gmail.com>
2021-05-25 15:49:40 -0700

    Passing test.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const { static } = require('@architect/functions')
 
-module.exports = function staticImgSrc(md, opts) {
-  console.log('STATIC IMAGE SRC CALLED')
-  /*
-  loop over tokens
-  update img src with arc.static
-  if ('you find an img tag') {
-    img.src = static(img.src)
-  }
-  */
-  return md
+module.exports = function staticImgSrc(md) {
+  md.core.ruler.push('arc-static', state => {
+    const tokens = state.tokens
+    tokens.forEach((token) => {
+      if (token.type === 'inline') {
+        token.children.forEach(kid => {
+          const attrs = kid.attrs
+          attrs.forEach(attr => {
+            if(attr[0] === 'src')  {
+              attr[1] = static(attr[1])
+            }
+          })
+        })
+      }
+    })
+  })
 }
--- test.js
+++ test.js
@@ -12,7 +12,6 @@ test('should parse img', t => {
   const imgMD = '![My Image](myimage.jpg)'
   const result = md.render(imgMD)
   const expected = /_static\/myimage\.jpg/
-  //const actual = '<p><img src="_static/myimage.jpg" alt="My Image"></p>'
   t.ok(expected.test(result), result, 'Parsed image markdown')
   t.end()
 })
