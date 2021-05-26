4a64a4f6041ca6523eecc3fcf6195364c5dc16bd
kj <kristoferjoseph@gmail.com>
2021-05-25 22:12:14 -0700

    Cleans up tests.

--- test.js
+++ test.js
@@ -12,13 +12,13 @@ test('should update img src with arc static helper', t => {
   const imgMD = '![My Image](myimage.jpg)'
   const result = md.render(imgMD)
   const expected = /_static\/myimage\.jpg/
-  t.ok(expected.test(result), result, 'Parsed image markdown')
+  t.ok(expected.test(result), 'Updated img src attribute with arc.static helper')
   t.end()
 })
 
 test('should not blow up when inline element has no attributes', t => {
   const span = '<span></span>'
   const result = md.render(span)
-  t.ok(result)
+  t.ok(result, 'Did not esploder')
   t.end()
 })
