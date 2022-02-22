c1e111e072c951597e25dd9a8a7ce84fe7031e41
Ryan Block <rblock@gmail.com>
2022-02-21 18:06:36 -0800

    Update to Arc 10 / Functions 5

--- test.js
+++ test.js
@@ -1,24 +1,26 @@
 const test = require('tape')
+process.env.ARC_ENV = 'testing'
 const staticImgSrc = require('.')
 const md = require('markdown-it')()
   .use(staticImgSrc)
 
 test('Static img src should exist', t => {
+  t.plan(1)
   t.ok(staticImgSrc, 'staticImgSrc exists')
-  t.end()
 })
 
 test('should update img src with arc static helper', t => {
+  t.plan(1)
   const imgMD = '![My Image](myimage.jpg)'
   const result = md.render(imgMD)
   const expected = /_static\/myimage\.jpg/
   t.ok(expected.test(result), 'Updated img src attribute with arc.static helper')
-  t.end()
 })
 
 test('should not blow up when inline element has no attributes', t => {
+  t.plan(1)
   const span = '<span></span>'
   const result = md.render(span)
   t.ok(result, 'Did not esploder')
-  t.end()
+  delete process.env.ARC_ENV
 })
